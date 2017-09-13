885bbe82d0bb092a8e496395c93c5d83210b581b
Ross Basarevych <basarevych@gmail.com>
2017-09-14 00:56:56 +0300

    Initial commit

---
+++ config/global.js
@@ -0,0 +1,15 @@
+/**
+ * Repo-saved module configuration
+ */
+module.exports = {
+    // Load module classes and services, path names
+    autoload: [
+        'module.js',
+        'servers',
+    ],
+
+    // Translation directories
+    i18n: [
+        'i18n',
+    ],
+};
