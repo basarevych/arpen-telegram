ddc594666807798abe5ab9ec868267201fe22b5f
Ross Basarevych <basarevych@gmail.com>
2017-10-03 13:40:03 +0300

    Commander fix

--- src/services/commander.js
+++ src/services/commander.js
@@ -2,6 +2,7 @@
  * Telegram command handler
  * @module telegram/services/commander
  */
+const moment = require('moment-timezone');
 const natural = require('natural');
 const NError = require('nerror');
 
@@ -110,6 +111,7 @@ class Commander {
      * @return {*}
      */
     match(input, syntax) {
+        input = input.toLowerCase();
         let results = {};
         for (let variant of Object.keys(syntax)) {
             results[variant] = {};
@@ -147,12 +149,129 @@ class Commander {
     }
 
     /**
+     * Check if input contains anything of search
+     * @param {string} locale
+     * @param {string} input
+     * @param {string} search
+     * @return {boolean}
+     */
+    hasAny(locale, input, search) {
+        let inputTokens = this.stem(locale, input);
+        let searchTokens = this.stem(locale, search);
+        for (let item of searchTokens) {
+            if (inputTokens.includes(item))
+                return true;
+        }
+        return false;
+    }
+
+    /**
+     * Extract date
+     * @param {string} locale
+     * @param {string} input
+     * @return {Promise}
+     */
+    async extractDate(locale, input) {
+        input = input.toLowerCase();
+        let result;
+        try {
+            let dateSyntax, sunday, monday, tuesday, wednesday, thursday, friday, saturday, yesterday, tomorrow, weekEnd;
+            if (locale === 'ru') {
+                dateSyntax = {
+                    iso: {
+                        main: /(\d\d\d\d)-(\d\d)-(\d\d)/
+                    },
+                    long_locale_1: {
+                        main: /(\d\d)\.(\d\d)\.(\d\d\d\d)/
+                    },
+                    long_locale_2: {
+                        main: /(\d\d)\/(\d\d)\/(\d\d\d\d)/
+                    },
+                    short_locale_1: {
+                        main: /(\d\d)\.(\d\d)/
+                    },
+                    short_locale_2: {
+                        main: /(\d\d)\/(\d\d)/
+                    },
+                };
+                sunday = 'воскресенье';
+                monday = 'понедельник';
+                tuesday = 'вторник';
+                wednesday = 'среда';
+                thursday = 'четверг';
+                friday = 'пятница';
+                saturday = 'суббота';
+                yesterday = 'вчера';
+                tomorrow = 'завтра';
+                weekEnd = 'конец недели';
+            }
+
+            let now = moment();
+            let match;
+            if (dateSyntax)
+                match = this.match(input, dateSyntax);
+            if (match) {
+                let year, month, day;
+                if (match.iso) {
+                    year = parseInt(match.iso.main[1]);
+                    month = parseInt(match.iso.main[2]);
+                    day = parseInt(match.iso.main[3]);
+                } else if (match.long_locale_1) {
+                    year = parseInt(match.long_locale_1.main[3]);
+                    month = parseInt(match.long_locale_1.main[2]);
+                    day = parseInt(match.long_locale_1.main[1]);
+                } else if (match.long_locale_2) {
+                    year = parseInt(match.long_locale_2.main[3]);
+                    month = parseInt(match.long_locale_2.main[2]);
+                    day = parseInt(match.long_locale_2.main[1]);
+                } else if (match.short_locale_1) {
+                    year = now.year();
+                    month = parseInt(match.short_locale_1.main[2]);
+                    day = parseInt(match.short_locale_1.main[1]);
+                } else if (match.short_locale_2) {
+                    year = now.year();
+                    month = parseInt(match.short_locale_2.main[2]);
+                    day = parseInt(match.short_locale_2.main[1]);
+                }
+                if (year && month && day) {
+                    if (year < 1000)
+                        year += 2000;
+                    result = moment(`${year}-${month.toString().padStart(2, '0')}-${day.toString().padStart(2, '0')} 00:00:00`);
+                }
+            } else {
+                if (sunday && this.hasAll(locale, input, sunday))
+                    result = moment().day('Sunday');
+                else if (monday && this.hasAll(locale, input, monday))
+                    result = moment().day('Monday');
+                else if (tuesday && this.hasAll(locale, input, tuesday))
+                    result = moment().day('Tuesday');
+                else if (wednesday && this.hasAll(locale, input, wednesday))
+                    result = moment().day('Wednesday');
+                else if (thursday && this.hasAll(locale, input, thursday))
+                    result = moment().day('Thursday');
+                else if ((friday && this.hasAll(locale, input, friday)) || (weekEnd && this.hasAll(locale, input, weekEnd)))
+                    result = moment().day('Friday');
+                else if (saturday && this.hasAll(locale, input, saturday))
+                    result = moment().day('Saturday');
+                else if (yesterday && this.hasAll(locale, input, yesterday))
+                    result = now.subtract(1, 'days');
+                else if (tomorrow && this.hasAll(locale, input, tomorrow))
+                    result = now.add(1, 'days');
+            }
+        } catch (error) {
+            return null;
+        }
+        return (result && moment.isMoment(result)) ? result : null;
+    }
+
+    /**
      * Tokenize and stem
      * @param {string} locale
      * @param {string} input
      * @return {string[]}
      */
     stem(locale, input) {
+        input = input.toLowerCase();
         if (!locale)
             locale = this._i18n.defaultLocale;
         if (locale === 'ru')
